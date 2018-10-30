2aeaf7bfbd360b3c47004cbab499086e7c8fffa9
Ian Yake <iangyake@gmail.com>
2018-10-30 14:03:39 -0500

    test size of empty list

--- eecs448-lab06-base/TestSuite.h
+++ eecs448-lab06-base/TestSuite.h
@@ -3,6 +3,7 @@
 *	@date 10/20/18
 *	@brief A header file for a testing class
 */
+#include "LinkedListOfInts.h"
 
 #ifndef TESTER_H
 #define TESTER_H
@@ -20,20 +21,13 @@ class TestSuite
 	*   @post print out the success of tests on the linkeed list
 	*/
   void runTests();
-  /*
+  
   bool test1();
-  bool test2();
-  bool test3();
-  bool test4();
-  bool test5();
-  bool test6();
-  bool test7();
-  bool test8();
-  bool test9();
-  bool test10();
-  bool test11();*/
+  
 
 	private:
+  
+  int size;
 };
 
 #include "TestSuite.hpp"
--- eecs448-lab06-base/TestSuite.hpp
+++ eecs448-lab06-base/TestSuite.hpp
@@ -3,6 +3,7 @@
 *	@date 10/30/18
 *	@file TestSuite.hpp
 */
+#include <iostream>
 
 TestSuite::TestSuite()
 {
@@ -11,5 +12,12 @@ TestSuite::TestSuite()
 
 void TestSuite::runTests()
 {
-  std::cout << "testing\n";
+  std::cout << "Test 1: size of empty list is zero: " << (test1() ? "PASSED" : "FAILED") << "\n";
+}
+
+bool TestSuite::test1()
+{
+  LinkedListOfInts testableList;
+  size = 0;
+  return(testableList.isEmpty() == size);
 }
