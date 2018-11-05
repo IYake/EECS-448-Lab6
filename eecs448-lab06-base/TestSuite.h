da1c3899c3cd139d1f0754de5737025d1455147e
Ian Yake <iangyake@gmail.com>
2018-11-05 16:30:50 -0600

    add up to test 8

--- eecs448-lab06-base/TestSuite.h
+++ eecs448-lab06-base/TestSuite.h
@@ -25,6 +25,11 @@ class TestSuite
   bool test1();
 	bool test2();
 	bool test3();
+	bool test4();
+	bool test5();
+	bool test6();
+	bool test7();
+	bool test8();
   
 
 	private:
--- eecs448-lab06-base/TestSuite.hpp
+++ eecs448-lab06-base/TestSuite.hpp
@@ -12,9 +12,14 @@ TestSuite::TestSuite()
 
 void TestSuite::runTests()
 {
-  std::cout << "Test 1: Initial list is empty: " << (test1() ? "PASSED" : "FAILED") << "\n";
-  std::cout << "Test 2: Initial list is size 0: " << (test2() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 1: isEmpty for initial list is true: " << (test1() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 2: size of initial list is 0: " << (test2() ? "PASSED" : "FAILED") << "\n";
   std::cout << "Test 3: addFront adds correct value to empty list: " << (test3() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 4: isEmpty returns false after one addFront: " << (test4() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 5: size of list after one addFront is 1: " << (test5() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 6: addBack adds correct value to empty list: " << (test6() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 7: isEmpty returns false after one addBack: " << (test7() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 8: size of list after one addBack is 1: " << (test8() ? "PASSED" : "FAILED") << "\n";
 }
 
 bool TestSuite::test1()
@@ -33,8 +38,42 @@ bool TestSuite::test2()
 bool TestSuite::test3()
 {
   LinkedListOfInts testableList;
-  // printout the list, and then see if it's the expected print out
   testableList.addFront(23);
   return (testableList.toVector().at(0) == 23);
-  
 }
+
+bool TestSuite::test4()
+{
+  LinkedListOfInts testableList;
+  testableList.addFront(23);
+  return (testableList.isEmpty() == false);
+}
+
+bool TestSuite::test5()
+{
+  LinkedListOfInts testableList;
+  testableList.addFront(23);
+  return (testableList.size() == 1);
+}
+
+bool TestSuite::test6()
+{
+  LinkedListOfInts testableList;
+  testableList.addBack(23);
+  return (testableList.toVector().at(0) == 23);
+}
+
+bool TestSuite::test7()
+{
+  LinkedListOfInts testableList;
+  testableList.addBack(23);
+  return (testableList.isEmpty() == false);
+}
+
+bool TestSuite::test8()
+{
+  LinkedListOfInts testableList;
+  testableList.addBack(23);
+  return (testableList.size() == 1);
+}
+
