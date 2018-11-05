853ef9615c72448e422918a75308c5bc6523adba
Ian Yake <iangyake@gmail.com>
2018-11-05 16:55:07 -0600

    add up to test 18

--- eecs448-lab06-base/TestSuite.h
+++ eecs448-lab06-base/TestSuite.h
@@ -30,7 +30,16 @@ class TestSuite
 	bool test6();
 	bool test7();
 	bool test8();
-  
+	bool test9();
+	bool test10();
+	bool test11();
+	bool test12();
+	bool test13();
+	bool test14();
+	bool test15();
+	bool test16();
+  bool test17();
+	bool test18();
 
 	private:
   
--- eecs448-lab06-base/TestSuite.hpp
+++ eecs448-lab06-base/TestSuite.hpp
@@ -20,6 +20,16 @@ void TestSuite::runTests()
   std::cout << "Test 6: addBack adds correct value to empty list: " << (test6() ? "PASSED" : "FAILED") << "\n";
   std::cout << "Test 7: isEmpty returns false after one addBack: " << (test7() ? "PASSED" : "FAILED") << "\n";
   std::cout << "Test 8: size of list after one addBack is 1: " << (test8() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 9: list is empty after one addFront and one removeFront: " << (test9() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 10: list is empty after one addFront and one removeBack: " << (test10() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 11: isEmpty is true after one addFront and one removeFront: " << (test11() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 12: isEmpty is true after one addFront and one removeBack: " << (test12() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 13: removeFront after an addFront returns the true size to 0: " << (test13() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 14: removeBack after an addFront returns the true size to 0: " << (test14() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 15: size is 0 after an addFront and a removeFront: " << (test15() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 16: size is 0 after an addFront and a removeBack: " << (test16() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 17: size is 4 after 4 addFronts: " << (test17() ? "PASSED" : "FAILED") << "\n";
+  std::cout << "Test 18: size is 4 after 4 addBacks: " << (test18() ? "PASSED" : "FAILED") << "\n";
 }
 
 bool TestSuite::test1()
@@ -76,4 +86,106 @@ bool TestSuite::test8()
   testableList.addBack(23);
   return (testableList.size() == 1);
 }
+
+bool TestSuite::test9() //see if vector is empty after removeFront
+{
+  LinkedListOfInts testableList;
+  testableList.addFront(23);
+  testableList.removeFront();
+  return (testableList.toVector().empty() == true);
+}
+
+bool TestSuite::test10()//see if vector is empty after removeBack
+{
+  LinkedListOfInts testableList;
+  testableList.addFront(23);
+  testableList.removeBack();
+  return (testableList.toVector().empty() == true);
+}
+
+bool TestSuite::test11()//see if vector isEmpty is true after removeFront
+{
+  LinkedListOfInts testableList;
+  testableList.addFront(23);
+  testableList.removeFront();
+  return (testableList.isEmpty() == true);
+}
+
+bool TestSuite::test12()//see if vector isEmpty is true after removeBack
+{
+  LinkedListOfInts testableList;
+  testableList.addFront(23);
+  testableList.removeBack();
+  return (testableList.isEmpty() == true);
+}
+
+bool TestSuite::test13()//see if size is 0 after removeFront
+{
+  LinkedListOfInts testableList;
+  testableList.addFront(23);
+  testableList.removeFront();
+  return (testableList.toVector().size() == 0);
+}
+
+bool TestSuite::test14()//see if size is 0 after removeBack
+{
+  LinkedListOfInts testableList;
+  testableList.addFront(23);
+  testableList.removeBack();
+  return (testableList.toVector().size() == 0);
+}
+
+bool TestSuite::test15()//see if size returns 0 after removeFront
+{
+  LinkedListOfInts testableList;
+  testableList.addFront(23);
+  testableList.removeFront();
+  return (testableList.size() == 0);
+}
+
+bool TestSuite::test16()//see if size returns 0 after removeBack
+{
+  LinkedListOfInts testableList;
+  testableList.addFront(23);
+  testableList.removeBack();
+  return (testableList.size() == 0);
+}
+
+bool TestSuite::test17()//see if size returns 4 after 4 addFronts
+{
+  LinkedListOfInts testableList;
+  testableList.addFront(23);
+  testableList.addFront(24);
+  testableList.addFront(25);
+  testableList.addFront(26);
+  return (testableList.size() == 4);
+}
+
+bool TestSuite::test18()//see if size returns 4 after 4 addBacks
+{
+  LinkedListOfInts testableList;
+  testableList.addBack(23);
+  testableList.addBack(24);
+  testableList.addBack(25);
+  testableList.addBack(26);
+  return (testableList.size() == 4);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
